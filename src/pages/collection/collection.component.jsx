91775153fd79e9979053693b9ee5748b0c91b067
Augusto Acual <augustoacual@gmail.com>
2023-01-09 21:23:55 -0600

    Collection Routing and Selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,9 +1,13 @@
+import { connect } from 'react-redux'
+
 import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './collecton.styles.scss'
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId)
+const CollectionPage = ({ collection }) => {
+  console.log(collection)
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -11,4 +15,8 @@ const CollectionPage = ({ match }) => {
   )
 }
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+})
+
+export default connect(mapStateToProps)(CollectionPage)
